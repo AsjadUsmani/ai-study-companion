6fdfb78544007a9c5ca2cddd1293fc04b99bf050
Asjad Usmani <asjadusmani4@gmail.com>
2025-12-30 23:05:36 +0530

    remove window event and add useRouter

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 export default function Navbar() {
   const handleLogout = async () => {
+    const router = useRouter()
     await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/logout` ||"http://localhost:5000/auth/logout", {
       method: "POST",
       credentials: "include",
     })
-    console.log(window.location);
-    window.location.pathname = "/login"
+    router.push("/login")
   }
   return (
     <nav className="flex gap-6 p-4 border-b">
