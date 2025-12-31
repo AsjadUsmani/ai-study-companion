f0a50041bc34756404187e656c38ed9d2e30bc46
Asjad Usmani <asjadusmani4@gmail.com>
2025-12-31 16:39:42 +0530

    Gemini api for summary and backend connect to python for summarization

---
+++ apps/ai/prompts.py
@@ -0,0 +1,10 @@
+def summarize_prompt():
+    return f"""
+You are a study assistant.
+    Summarize the provided study notes clearly and concisely.
+    Focus on key concepts, important definitions, and main ideas.
+    Use simple language and avoid unnecessary details.
+    Output in plain text (no markdown).
+    
+    Also give in paragraphs and do not add '/n' 
+"""
